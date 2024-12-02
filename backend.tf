3388abb422b667150c4843bc2de2162b82516eb5
raja.sp <nichecloudexpert@gmail.com>
2024-12-03 01:07:27 +0530

    Changes 2

--- backend.tf
+++ backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "gcs" {
+    
     bucket = "tftask2024"
     #bucket = "quantacobucket"
     prefix = "terraform/global"
-    project = "your-gcp-project-id"
+    #project = "your-gcp-project-id"
     #credentials = "./quantaco-443301-90aa0b1191d8.json"
+
   }
 }
