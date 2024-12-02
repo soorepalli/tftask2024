b06a87ed10571a02997ca971413f819480342de7
raja.sp <nichecloudexpert@gmail.com>
2024-12-03 02:34:24 +0530

    task5

--- users/user1_backend.tf
+++ users/user1_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "terraform-state-bucket-user1"
+    bucket  = "tftk2024"
     prefix  = "terraform/state/user1"
     #project = "your-gcp-project-id"
   }
